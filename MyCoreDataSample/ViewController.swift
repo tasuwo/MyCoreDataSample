d24dc19d9d3c41dc26fa3bb7e1e825bed0dc3f85
tasuwo <kamuhata.you@gmail.com>
2020-11-03 20:09:48 +0900

    :tada: Initial commit.

---
+++ MyCoreDataSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyCoreDataSample
+//
+//  Created by Tasuku Tozawa on 2020/11/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
